17063c46f93051f9ac1c8178de4a3579ce00b356
xi4 <beelzebul.18@gmail.com>
2016-09-09 15:38:57 +0400

    растения

---
+++ add_herb.py
@@ -0,0 +1,279 @@
+# coding=utf-8
+import db
+
+db.Herb.create(txt="Укроп",lvl=0,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Петрушка",lvl=1,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Лук",lvl=2,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Хрен",lvl=2,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Мята",lvl=3,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Редис",lvl=3,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Капуста",lvl=4,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Базилик",lvl=4,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Морковь",lvl=5,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Свекла",lvl=5,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Щавель",lvl=6,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Картофель",lvl=6,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Горох",lvl=7,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Перец",lvl=7,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Томат",lvl=8,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Сельдерей",lvl=8,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Брокколи",lvl=9,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Огурец",lvl=9,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Кабачок",lvl=10,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="Клубника",lvl=10,price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+db.Herb.create(txt="",lvl="",price=1,g_time=1,min=1,max=1)
+
+Укроп, цена: o 10
+
+Петрушка, доступно с 1 уровня
+
+Лук, доступно с 2 уровня
+
+Хрен, доступно с 2 уровня
+
+Мята, доступно с 3 уровня
+
+Редис, доступно с 3 уровня
+
+Капуста, доступно с 4 уровня
+
+Базилик, доступно с 4 уровня
+
+Морковь, доступно с 5 уровня
+
+Свекла, доступно с 5 уровня
+
+Щавель, доступно с 6 уровня
+
+Картофель, доступно с 6 уровня
+
+Горох, доступно с 7 уровня
+
+Перец, доступно с 7 уровня
+
+Томат, доступно с 8 уровня
+
+Сельдерей, доступно с 8 уровня
+
+Брокколи, доступно с 9 уровня
+
+Огурец, доступно с 9 уровня
+
+Кабачок, доступно с 10 уровня
+
+Клубника, доступно с 10 уровня
+
+Фасоль, доступно с 11 уровня
+
+Баклажан, доступно с 11 уровня
+
+Тыква, доступно с 12 уровня
+
+Арбуз, доступно с 12 уровня
+
+Чеснок, доступно с 13 уровня
+
+Спаржа, доступно с 13 уровня
+
+Кукуруза, доступно с 14 уровня
+
+Патиссон, доступно с 14 уровня
+
+Чили, доступно с 15 уровня
+
+Дыня, доступно с 15 уровня
+
+Ежевика, доступно с 16 уровня
+
+Яблоко, доступно с 16 уровня
+
+Крыжовник, доступно с 17 уровня
+
+Груша, доступно с 17 уровня
+
+Виноград, доступно с 18 уровня
+
+Бананы, доступно с 18 уровня
+
+Малина, доступно с 19 уровня
+
+Грибы, доступно с 19 уровня
+
+Вишня, доступно с 20 уровня
+
+Подсолнух, доступно с 20 уровня
+
+Анютины Глазки, доступно с 21 уровня
+
+Гибискусы, доступно с 22 уровня
+
+Ландыши, доступно с 23 уровня
+
+Колокольчики, доступно с 24 уровня
+
+Тюльпаны, доступно с 25 уровня
+
+Лилии, доступно с 26 уровня
+
+Гвоздики, доступно с 27 уровня
+
+Ромашки, доступно с 28 уровня
+
+Маки, доступно с 29 уровня
+
+Розы, доступно с 30 уровня
+
+Арахис, доступно с 31 уровня
+
+Миндаль, доступно с 32 уровня
+
+Фундук, доступно с 33 уровня
+
+Кедровый орех, доступно с 34 уровня
+
+Кокос, доступно с 35 уровня
+
+Лимон, доступно с 36 уровня
+
+Смородина, доступно с 36 уровня
+
+Слива, доступно с 37 уровня
+
+Облепиха, доступно с 37 уровня
+
+Персик, доступно с 38 уровня
+
+Черника, доступно с 38 уровня
+
+Абрикос, доступно с 39 уровня
+
+Брусника, доступно с 39 уровня
+
+Апельсин, доступно с 40 уровня
+
+Гранат, доступно с 40 уровня
+
+Финики, доступно с 41 уровня
+
+Хурма, доступно с 42 уровня
+
+Киви, доступно с 43 уровня
+
+Манго, доступно с 44 уровня
+
+Айва, доступно с 45 уровня
+
+Барбарис, доступно с 46 уровня
+
+Ирис, доступно с 46 уровня
+
+Календула, доступно с 47 уровня
+
+Крапива, доступно с 47 уровня
+
+Кувшинка, доступно с 48 уровня
+
+Одуванчик, доступно с 48 уровня
+
+Подорожник, доступно с 49 уровня
+
+Сирень, доступно с 49 уровня
+
+Василёк, доступно с 50 уровня
+
+Женьшень, доступно с 50 уровня
+
+Гречиха, доступно с 51 уровня
+
+Овёс, доступно с 52 уровня
+
+Рожь, доступно с 53 уровня
+
+Пшеница, доступно с 54 уровня
+
+Авокадо, доступно с 55 уровня
+
+Имбирь, доступно с 57 уровня
+
+Маракуйя, доступно с 59 уровня
+
+Папайя, доступно с 60 уровня
+
+Репа, доступно с 70 уровня
+
+Соя, доступно с 80 уровня
+
+Цветная капуста, доступно с 90 уровня
+
+Оливки, доступно с 100 уровня
+
+Каштан, доступно с 105 уровня
+
+Ананас, доступно с 110 уровня
+
+Грейпфрут, доступно с 115 уровня
+
+Лайм, доступно с 120 уровня
